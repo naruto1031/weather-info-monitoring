6530243111f215daff0421e6ae7786bf52a09bba
naruto1031 <naruto0614@au.com>
2024-01-29 00:40:00 +0900

    fix: 正常にfirebaseがinitされない問題を修正

--- src/pages/api/coordinate.ts
+++ src/pages/api/coordinate.ts
@@ -1,25 +1,27 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Coordinate } from '@/type/apiSchema'
-import type { NextApiRequest, NextApiResponse } from 'next'
-const { cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require('../../../weather-info-monitoring-firebase.json'); // 秘密鍵を取得
-const admin = require('firebase-admin');
+import { Coordinate } from "@/type/apiSchema";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { cert } from "firebase-admin/app";
+import serviceAccount from "../../../weather-info-monitoring-firebase.json";
+import admin from "firebase-admin";
+admin.initializeApp({
+  credential: cert(serviceAccount as admin.ServiceAccount),
+});
+import { getFirestore } from "firebase-admin/firestore";
+const db = getFirestore();
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Coordinate>
 ) {
+  const COLLECTION_NAME = "coordinate";
+  const targetDoc = "current";
 
-  const COLLECTION_NAME = 'coordinate'
-  const targetDoc="current"
-  if (admin.apps.length === 0) {
-    admin.initializeApp({
-      credential: cert(serviceAccount),
-    });
+  const doc = await db.collection(COLLECTION_NAME).doc(targetDoc).get();
+  const data = doc.data();
+
+  if (!data) {
+    return res.status(404).json({ error: "Not found" } as any);
   }
 
-  const db = getFirestore();
-  const doc = await db.collection(COLLECTION_NAME).doc(targetDoc).get();
-  res.status(200).json(doc.data());
+  res.status(200).json(data as Coordinate);
 }
